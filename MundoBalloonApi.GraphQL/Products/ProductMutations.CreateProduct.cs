31806f59ac4a4473b44d7a6386d721847b16e612
Luis Gonzalez <luis.gonzalez@airbnb.com>
2021-11-01 12:19:14 -0700

    Added a quick authorize and updated some packages

--- MundoBalloonApi.GraphQL/Products/ProductMutations.CreateProduct.cs
+++ MundoBalloonApi.GraphQL/Products/ProductMutations.CreateProduct.cs
@@ -1,10 +1,12 @@
 using HotChocolate;
+using HotChocolate.AspNetCore.Authorization;
 using MundoBalloonApi.business.Contracts;
 using MundoBalloonApi.business.DataObjects.Requests;
 using MundoBalloonApi.graphql.Products.Requests;
 
 namespace MundoBalloonApi.graphql.Products
 {
+    [Authorize]
     public partial class ProductMutations
     {
         public CreateProductPayload CreateProduct(
