163066a67d5b2c6e79025035c73598616a14d38d
Luis Gonzalez <luis.gonzalez@airbnb.com>
2022-08-29 12:02:54 -0700

    Sync db stuff

--- MundoBalloonApi.infrastructure/Data/Models/EventCartDetail.cs
+++ MundoBalloonApi.infrastructure/Data/Models/EventCartDetail.cs
@@ -2,12 +2,9 @@
 
 public class EventCartDetail : BaseEntity
 {
-    public int EventCartId { get; init; }
-    public string? Sku { get; init; }
+    public int EventCartDetailId { get; init; }
     public int ProductVariantId { get; init; }
     public double Quantity { get; init; }
-    public double Price { get; init; }
-    public string? Label { get; init; }
     public int? UserEventId { get; init; }
 
     public ProductVariant? ProductVariant { get; set; }
