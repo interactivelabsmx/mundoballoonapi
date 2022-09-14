d89072102af55fe4f8d65a262eaed875673ac565
Chris_Santillan <santillan.silva.christian@gmail.com>
2022-09-13 20:31:40 -0500

    Create UserEventQueries.GetUserEventByUserId.cs

---
+++ MundoBalloonApi.GraphQL/Users/UserEventQueries.GetUserEventByUserId.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using MundoBalloonApi.infrastructure.Data.Models;
+using UserEvent = MundoBalloonApi.business.DTOs.Entities.UserEvent;
+
+namespace MundoBalloonApi.graphql.Users;
+
+public partial class UserEventQueries
+{
+    [AllowAnonymous]
+    [UseDbContext(typeof(MundoBalloonContext))]
+    public IQueryable<UserEvent> GetUserEventByUserId([ScopedService] MundoBalloonContext mundoBalloonContext,
+        [Service] IMapper mapper, string userId)
+    {
+        return mapper.ProjectTo<UserEvent>(mundoBalloonContext.UserEvents);
+    }
+}
