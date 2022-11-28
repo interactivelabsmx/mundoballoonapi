b017e8fb0885f8e51bea80af06169b9333ea0b3e
Chris_Santillan <santillan.silva.christian@gmail.com>
2022-11-27 22:23:58 -0600

    Create UserEventQueries.GetUserCart.cs

---
+++ MundoBalloonApi.GraphQL/Users/UserEventQueries.GetUserCart.cs
@@ -0,0 +1,28 @@
+using AutoMapper;
+using HotChocolate.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using MundoBalloonApi.business.DTOs.Entities;
+using MundoBalloonApi.infrastructure.Data.Models;
+using UserCart = MundoBalloonApi.business.DTOs.Entities.UserCart;
+
+namespace MundoBalloonApi.graphql.Users;
+
+public partial class UserEventQueries
+{
+    [AllowAnonymous]
+    [UseDbContext(typeof(MundoBalloonContext))]
+    public IQueryable<UserCart> GetUserCart([ScopedService] MundoBalloonContext mundoBalloonContext,
+        [Service] IMapper mapper, [GlobalState("currentUser")] CurrentUser currentUser)
+    {
+       /* var userCart = await(
+            from cart in mundoBalloonContext.UserCarts
+            where cart.UserId == userId
+            select cart
+        ).Include(uc => uc.ProductVariant)
+        .AsSplitQuery()
+        .AsNoTracking()
+        .FirstOrDefaultAsync(uc => uc.UserId == userId);*/
+        return mapper.ProjectTo<UserCart>(mundoBalloonContext.UserCarts.Where(uc => uc.UserId == currentUser.UserId).Include(uc => uc.ProductVariant));
+    }
+}
